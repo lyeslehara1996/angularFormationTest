1376681001a9ed3a61511c1c033ffe91fc43f306
LyesLehara <lyeslehara88@gmail.com>
2023-01-05 14:47:26 +0100

    project angular testing

---
+++ src/app/Components/hotels-list/hotels-list.component.ts
@@ -0,0 +1,61 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-hotels-list',
+  templateUrl: './hotels-list.component.html',
+  styleUrls: ['./hotels-list.component.css']
+})
+export class HotelsListComponent {
+
+public title="List hotels"
+public showNouveau :boolean = false;
+
+public hotel :any=[
+  {
+        "hotelId": 1,
+        "hotelName": "Buea sweet life",
+        "description": "Belle vue au bord de la mer",
+        "price": 230.5,
+        "imageUrl": "../../../assets/images/indoors.jpg",
+      //  "imageUrl": "file:///C:/Users/pc/Desktop/Nouveau/AngPro/src/assets/images/hotel-room.jpg",
+        "rating": 3.5,
+        "showNouveau":false
+        
+  },
+  {
+        "hotelId": 2,
+        "hotelName": "Marakech",
+        "description": "Profitez de la vue sur les montagnes",
+        "price": 145.5,
+           "imageUrl": "../../../assets/images/indoors.jpg",
+        "rating": 5,
+        "showNouveau":true
+        
+  },
+  {
+        "hotelId": 3,
+        "hotelName": "Abudja new look palace",
+        "description": "Séjour complet avec service de voitures",
+        "price": 120.12,
+        "imageUrl": "../../../assets/images/the-interior.jpg",
+        "rating": 4,
+        "showNouveau":false
+        
+  },
+  {
+        "hotelId": 4,
+        "hotelName": "Cape town city",
+        "description": "Magnifique cadre pour votre séjour",
+        "price": 135.12,
+           "imageUrl": "../../../assets/images/window.jpg",
+        "rating": 2.5,
+        "showNouveau":true
+        
+  }
+]
+
+afficherNouveau(){
+this.showNouveau = !this.showNouveau
+}
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HotelsListComponent } from './Components/hotels-list/hotels-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HotelsListComponent
   ],
   imports: [
     BrowserModule
@@ -13,4 +15,8 @@ import { AppComponent } from './app.component';
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+
+
+
+ }
