5338838bab162ce5f4137021cb5f9f138b463053
LyesLehara <lyeslehara88@gmail.com>
2023-01-11 08:54:34 +0100

    add interface ihotel

--- src/app/Components/hotels-list/hotels-list.component.ts
+++ src/app/Components/hotels-list/hotels-list.component.ts
@@ -2,7 +2,7 @@ import { registerLocaleData } from '@angular/common';
 import { Component } from '@angular/core';
 import localeFr from '@angular/common/locales/fr';
 import { IHotel } from 'src/app/Models/IHotel';
-registerLocaleData(localeFr,'fr')
+registerLocaleData(localeFr, 'fr')
 @Component({
   selector: 'app-hotels-list',
   templateUrl: './hotels-list.component.html',
@@ -10,60 +10,60 @@ registerLocaleData(localeFr,'fr')
 })
 export class HotelsListComponent {
 
-public title="List hotels"
-public showNouveau :boolean = false;
-public hotelfilter="mot"
+  public title = "List hotels"
+  public showNouveau: boolean = false;
+  public hotelfilter = "mot"
 
 
-public hotel :IHotel[]=[
-  {
-        "hotelId": 1,
-        "hotelName": "Buea sweet life",
-        "description": "Belle vue au bord de la mer",
-        "price": 230.5,
+  public hotel: IHotel[] = [
+    {
+      "hotelId": 1,
+      "hotelName": "Buea sweet life",
+      "description": "Belle vue au bord de la mer",
+      "price": 230.5,
       "imageUrl": "../../../assets/images/indoors.jpg",
       //"imageUrl": "file:///C:/Users/pc/Desktop/Nouveau/AngPro/src/assets/images/hotel-room.jpg",  
       "rating": 3.5,
-        "showNouveau":false
-        
-  },
-  {
-        "hotelId": 2,
-        "hotelName": "Marakech",
-        "description": "Profitez de la vue sur les montagnes",
-        "price": 145.5,
-           "imageUrl": "../../../assets/images/indoors.jpg",
-         // "imageUrl": "file:///C:/Users/pc/Desktop/Nouveau/AngPro/src/assets/images/indoors.jpg",
-           "rating": 5,
-        "showNouveau":true
-        
-  },
-  {
-        "hotelId": 3,
-        "hotelName": "Abudja new look palace",
-        "description": "Séjour complet avec service de voitures",
-        "price": 120.12,
-        "imageUrl": "../../../assets/images/the-interior.jpg",
-         // "imageUrl": "file:///C:/Users/pc/Desktop/Nouveau/AngPro/src/assets/images/the-interior.jpg",
-        "rating": 4,
-        "showNouveau":false
-        
-  },
-  {
-        "hotelId": 4,
-        "hotelName": "Cape town city",
-        "description": "Magnifique cadre pour votre séjour",
-        "price": 135.12,
-           "imageUrl": "../../../assets/images/window.jpg",
-         // "imageUrl": "file:///C:/Users/pc/Desktop/Nouveau/AngPro/src/assets/images/window.jpg",
-           "rating": 2.5,
-        "showNouveau":true
-        
-  }
-]
+      "showNouveau": false
 
-afficherNouveau(){
-this.showNouveau = !this.showNouveau
-}
+    },
+    {
+      "hotelId": 2,
+      "hotelName": "Marakech",
+      "description": "Profitez de la vue sur les montagnes",
+      "price": 145.5,
+      "imageUrl": "../../../assets/images/indoors.jpg",
+      // "imageUrl": "file:///C:/Users/pc/Desktop/Nouveau/AngPro/src/assets/images/indoors.jpg",
+      "rating": 5,
+      "showNouveau": true
+
+    },
+    {
+      "hotelId": 3,
+      "hotelName": "Abudja new look palace",
+      "description": "Séjour complet avec service de voitures",
+      "price": 120.12,
+      "imageUrl": "../../../assets/images/the-interior.jpg",
+      // "imageUrl": "file:///C:/Users/pc/Desktop/Nouveau/AngPro/src/assets/images/the-interior.jpg",
+      "rating": 4,
+      "showNouveau": false
+
+    },
+    {
+      "hotelId": 4,
+      "hotelName": "Cape town city",
+      "description": "Magnifique cadre pour votre séjour",
+      "price": 135.12,
+      "imageUrl": "../../../assets/images/window.jpg",
+      // "imageUrl": "file:///C:/Users/pc/Desktop/Nouveau/AngPro/src/assets/images/window.jpg",
+      "rating": 2.5,
+      "showNouveau": true
+
+    }
+  ]
+
+  afficherNouveau() {
+    this.showNouveau = !this.showNouveau
+  }
 
 }
